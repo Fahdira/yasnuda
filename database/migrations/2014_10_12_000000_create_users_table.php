d810f25f1599021567698fb81bb572d6ec5b5a95
Muhammad Fahdira <fahdiramhmmd@gmail.com>
2024-12-11 00:52:06 +0700

    Admin Panel

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('jabatan')->nullable();
+            $table->string('alamat')->nullable();
+            $table->string('no_telp', 20)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
