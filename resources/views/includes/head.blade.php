441d38610ae3fdedb678961dd32a18a649bb8e6b
AhenkReptile <kaifan606.sh@gmail.com>
2024-12-18 03:13:21 +0700

    Layout 2
    Adding favicon

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -4,4 +4,5 @@
     <title>Website Pendaftaran</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/>
+    <link rel="icon" href="{{ asset('images/logo/logo.png') }}">
 </head>
