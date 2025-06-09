e748218cbad8785c37bc78c609271fb4a58956f2
AhenkReptile <kaifan606.sh@gmail.com>
2025-06-10 03:30:48 +0700

    Push 9
    update link social media

--- resources/views/includes/about.blade.php
+++ resources/views/includes/about.blade.php
@@ -13,8 +13,13 @@
             <i class="fab fa-facebook text-2xl text-teal-600"></i>
             <div>
                 <h3 class="text-lg font-semibold">Facebook</h3>
-                <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fitenas.official&tabs=timeline&width=340&height=331&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId" width="316" height="296" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
-            </div>
+                    <a href="https://www.facebook.com/pages/Pondok%20Pesantren%20Nuurul%20Hudaa%20Lampegan/1953063928310402/">
+                        <img
+                            alt="facebook"
+                            class="w-[300px]"
+                            src="{{ asset('images/facebook/yasnuda.jpg') }}"
+                        />
+                    </a>            </div>
         </div>
         <div class="flex items-start space-x-4">
             <i class="fab fa-instagram text-2xl text-teal-600"></i>
@@ -31,7 +36,7 @@
                     <img alt="Instagram Image 8" class="w-full" height="100" src="https://placehold.co/100x100" width="100"/>
                     <img alt="Instagram Image 9" class="w-full" height="100" src="https://placehold.co/100x100" width="100"/>
                 </div> -->
-                <blockquote class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/DKHVtCxxuz8/" data-instgrm-version="14" style="max-width:540px; width:100%; margin:auto;">
+                <blockquote class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/CyrvS0gSW_n/" data-instgrm-version="14" style="max-height:300px; width:100%; margin:auto;">
                 </blockquote>
                 <script async src="//www.instagram.com/embed.js"></script>
             </div>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -10,15 +10,20 @@
         <div class="container mx-auto mx mt-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 items-center">
             @foreach ($posts as $post)
                 <div class="bg-white shadow-md rounded-lg overflow-hidden">
-                    <img
-                        alt="{{ $post->judul }}"
-                        class="w-full"
-                        src="{{ $post->getFirstMediaUrl('thumbnail', 'thumb') ?: 'https://placehold.co/300x200' }}"
-                        width="300"
-                        height="200"
-                    />
+                    <a href="{{ route('blog_isi', $post->slug) }}">
+                        <img
+                            alt="{{ $post->judul }}"
+                            class="w-full"
+                            src="{{ $post->getFirstMediaUrl('thumbnail', 'thumb') ?: 'https://placehold.co/300x200' }}"
+                            width="300"
+                            height="200"
+                        />
+                    </a>
                     <div class="p-4">
-                        <h2 class="text-lg font-semibold mb-2">{{ $post->judul }}</h2>
+                        <h2 class="text-lg font-semibold mb-2"><a href="{{ route('blog_isi', $post->slug) }}" class="text-blue-600 hover:underline">
+                                {{ $post->judul }}
+                            </a>
+                        </h2>
                         <p class="text-gray-600">{{ \Illuminate\Support\Str::limit($post->desc, 100) }}</p>
                         <small class="text-gray-400">Published by : {{ $post->user->name }} on {{ $post->created_at }}</small>
                     </div>
@@ -30,7 +35,7 @@
         <h2 class="text-2xl font-semibold text-center mb-8">Peta Lokasi</h2>
         <div class="flex justify-center">
             <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.944398653029!2d107.63497627481875!3d-6.89725396749632!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e7bb3ecbe445%3A0xc603604dc4fa418!2sITENAS%20GD%202!5e0!3m2!1sen!2sid!4v1748445627908!5m2!1sen!2sid"
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1013572.1929717141!2d106.7425632!3d-7.0950915!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6845ca1cef7073%3A0x8fc9e5ad6b891073!2sYayasan%20Nuurul%20Hudaa!5e0!3m2!1sen!2sid!4v1749498495665!5m2!1sen!2sid"
                 width="1460"
                 height="600"
                 style="border:0;"
